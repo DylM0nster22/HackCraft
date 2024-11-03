0a998bddf3122885b9d6c33406e849017d226365
DylM0nster22 <tetherpro@gmail.com>
2024-11-02 17:26:09 -0700

    Initial commit

---
+++ src/main/java/net/dylan/magicmod/MagicMod.java
@@ -0,0 +1,16 @@
+package net.dylan.magicmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagicMod implements ModInitializer {
+	public static final String MOD_ID = "magicmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/dylan/magicmod/MagicModClient.java
@@ -0,0 +1,10 @@
+package net.dylan.magicmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
